617db730fba3e4ee4a001988ba9b01d99de5ac81
siky <41624527+SevenFo@users.noreply.github.com>
2025-02-24 17:20:45 +0800

    add comment

--- robotic_transformer_pytorch/robotic_transformer_pytorch.py
+++ robotic_transformer_pytorch/robotic_transformer_pytorch.py
@@ -69,6 +69,7 @@ class Residual(Module):
 
 class LayerNorm(Module):
     def __init__(self, dim):
+        """num_params = 2 * dim over an sample (batch-wised)"""
         super().__init__()
         self.gamma = nn.Parameter(torch.ones(dim))
         self.register_buffer("beta", torch.zeros(dim))
@@ -106,6 +107,9 @@ class FeedForward(Module):
 
 class SqueezeExcitation(Module):
     def __init__(self, dim, shrinkage_rate=0.25):
+        """
+        通道注意力机制，用于对不同通道进行加权，使用两层全连接层进行计算
+        """
         super().__init__()
         hidden_dim = int(dim * shrinkage_rate)
 
@@ -119,11 +123,14 @@ class SqueezeExcitation(Module):
         )
 
     def forward(self, x):
-        return x * self.gate(x)
+        # x     : b c h w
+        # gate  : b c 1 1
+        return x * self.gate(x)  # gate is the weight of each channel
 
 
 class MBConvResidual(Module):
     def __init__(self, fn, dropout=0.0):
+        """简简单单的残差连接模块，x = dropout(net(x)) + x"""
         super().__init__()
         self.fn = fn
         self.dropsample = Dropsample(dropout)
@@ -134,8 +141,9 @@ class MBConvResidual(Module):
         return out + x
 
 
-class Dropsample(Module):
+class Dropc(Module):
     def __init__(self, prob=0):
+        """用于drop 样本的模块，以概率prob丢弃输入的样本"""
         super().__init__()
         self.prob = prob
 
@@ -160,7 +168,7 @@ def MBConv(
 
     net = nn.Sequential(
         nn.Conv2d(dim_in, hidden_dim, 1),
-        nn.BatchNorm2d(hidden_dim),
+        nn.BatchNorm2d(hidden_dim),  # num_params = 2 * hidden_dim over mini-batch
         nn.GELU(),
         nn.Conv2d(
             hidden_dim, hidden_dim, 3, stride=stride, padding=1, groups=hidden_dim
@@ -359,6 +367,7 @@ class MaxViT(Module):
         dropout=0.1,
         channels=3,
     ):
+        """输入为 b c h w，输出为 b num_classes"""
         super().__init__()
         assert isinstance(depth, tuple), (
             "depth needs to be tuple if integers indicating number of transformer blocks at that stage"
@@ -408,6 +417,7 @@ class MaxViT(Module):
                         expansion_rate=mbconv_expansion_rate,
                         shrinkage_rate=mbconv_shrinkage_rate,
                     ),
+                    # 这里的 WindowAttention 感受野在 block 内部
                     Rearrange(
                         "b d (x w1) (y w2) -> b x y w1 w2 d", w1=w, w2=w
                     ),  # block-like attention
@@ -420,10 +430,11 @@ class MaxViT(Module):
                         )
                     ),
                     Residual(FeedForward(dim=layer_dim, dropout=dropout)),
+                    # 这里的 WindowAttention 感受野在 block 外部
                     Rearrange("b x y w1 w2 d -> b d (x w1) (y w2)"),
                     Rearrange(
                         "b d (w1 x) (w2 y) -> b x y w1 w2 d", w1=w, w2=w
-                    ),  # grid-like attention
+                    ),  # grid-like attention，先拆分成 w1*w2个 x*y 的小块，然后每个小块分别取相同位置的像素作为token(每个token为w1*w2个像素)，构成 x*y 个 token
                     Residual(
                         WindowAttention(
                             dim=layer_dim,
@@ -444,7 +455,7 @@ class MaxViT(Module):
         self.cond_hidden_dims = cond_hidden_dims
 
         # mlp head out
-
+        # mean pool and linear out
         self.mlp_head = nn.Sequential(
             Reduce("b d h w -> b d", "mean"),
             LayerNorm(embed_dim),
@@ -484,14 +495,15 @@ class MaxViT(Module):
 class TransformerAttention(Module):
     def __init__(
         self,
-        dim,
-        causal=False,
-        dim_head=64,
-        dim_context=None,
-        heads=8,
-        norm_context=False,
-        dropout=0.1,
+        dim,  # 输入token的维度
+        causal=False,  # 是否使用因果注意力（只能看到过去的信息）
+        dim_head=64,  # 每个注意力头的维度
+        dim_context=None,  # 上下文的维度（默认和输入token的维度相同），一般 key 和 value 由 context 生成
+        heads=8,  # 注意力头的数量
+        norm_context=False,  # 是否对上下文进行归一化
+        dropout=0.1,  # Dropout概率
     ):
+        """ """
         super().__init__()
         self.heads = heads
         self.scale = dim_head**-0.5
@@ -500,12 +512,13 @@ class TransformerAttention(Module):
 
         dim_context = default(dim_context, dim)
 
-        self.norm = LayerNorm(dim)
+        self.norm = LayerNorm(dim)  # 层归一化，即对一个样本的所有元素进行归一化
         self.context_norm = LayerNorm(dim_context) if norm_context else nn.Identity()
 
         self.attn_dropout = nn.Dropout(dropout)
 
         self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        # 键 k 和值 v 保持单头设计以节省参数（常见于高效Transformer变体）
         self.to_kv = nn.Linear(dim_context, dim_head * 2, bias=False)
         self.to_out = nn.Sequential(
             nn.Linear(inner_dim, dim, bias=False), nn.Dropout(dropout)
@@ -513,12 +526,12 @@ class TransformerAttention(Module):
 
     def forward(
         self,
-        x,
-        context=None,
-        mask=None,
-        attn_bias=None,
-        attn_mask=None,
-        cond_fn: Callable | None = None,
+        x,  # b, seq_len, dim
+        context=None,  # b, context_seq_len, dim_context
+        mask=None,  # 键值对的mask，b, seq_len
+        attn_bias=None,  # 附加到注意力矩阵上的偏置，b, heads, seq_len, context_seq_len
+        attn_mask=None,  # sim 使用的 mask，b, seq_len, context_seq_len
+        cond_fn: Callable | None = None,  # 条件函数
     ):
         b = x.shape[0]
 
@@ -545,18 +558,24 @@ class TransformerAttention(Module):
             sim = sim + attn_bias
 
         if exists(attn_mask):
-            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+            # 用最小注意力填充 mask == False 的位置
+            sim = sim.masked_fill_(~attn_mask, -torch.finfo(sim.dtype).max)
 
         if exists(mask):
+            # 所有和 mask == False 位置的 k,v 想乘法的 sim 都会被置为负无穷
             mask = rearrange(mask, "b j -> b 1 1 j")
-            sim = sim.masked_fill(~mask, -torch.finfo(sim.dtype).max)
+            sim = sim.masked_fill_(~mask, -torch.finfo(sim.dtype).max)
 
         if self.causal:
             i, j = sim.shape[-2:]
+            # 生成上三角掩码（i是查询长度，j是键长度，j - i + 1 为偏移量）
+            # 确保位置 i 只能关注到位置 j ≤ i 的键（即防止未来信息泄漏）
+            # 当 i = j（标准自回归场景）时，k = 1, 此时 triu(1) 会生成一个 严格上三角矩阵（不包含主对角线）
+            # 当 j > i 时, 假设 k,v 矩阵的 [:,0,j-i-1] 的 token 都是有用的（可能是记忆等等）需要被关注到
             causal_mask = torch.ones((i, j), dtype=torch.bool, device=x.device).triu(
                 j - i + 1
             )
-            sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+            sim = sim.masked_fill_(causal_mask, -torch.finfo(sim.dtype).max)
 
         attn = sim.softmax(dim=-1)
         attn = self.attn_dropout(attn)
@@ -606,11 +625,22 @@ class TokenLearner(Module):
     """
 
     def __init__(self, *, dim, ff_mult=2, num_output_tokens=8, num_layers=2):
+        """减少总的token数量，进行token压缩，最终输出 num_output_tokens 个 token，
+        其核心思想是 将 x 复制 num_output_tokens 份，使用 1x1 卷积对每个像素的各个通道进行分组特征提取，
+        输出 num_output_tokens 张权重图，然后使用这些权重图对复制的 x 进行加权求和，
+        最后将每组的所有通道的像素求均值，得到 num_output_tokens 个 token
+
+        输入：b c h w or b t c h w
+        输出：b c num_output_tokens or b t c num_output_tokens
+
+        实际上并没有对不同time_step之间的token进行特征压缩，而是只将一个time_step内的token进行压缩
+        """
         super().__init__()
         inner_dim = dim * ff_mult * num_output_tokens
 
         self.num_output_tokens = num_output_tokens
         self.net = nn.Sequential(
+            # 对于分组 conv，参数量为 k_1 * k_2 * c_in//g * c+out // g * g = k_1 * k_2 * c_in * c_out // g
             nn.Conv2d(dim * num_output_tokens, inner_dim, 1, groups=num_output_tokens),
             nn.GELU(),
             nn.Conv2d(inner_dim, num_output_tokens, 1, groups=num_output_tokens),
@@ -624,6 +654,8 @@ class TokenLearner(Module):
         attn = rearrange(attn, "b g h w -> b 1 g h w")
         x = rearrange(x, "b (g c) h w -> b c g h w", g=self.num_output_tokens)
 
+        # x * attn = b c g h w * b 1 g h w = b c g h w
+        # 注意 c 的大小代表了之后进行 attention 的 token 向量的维度
         x = reduce(x * attn, "b c g h w -> b c g", "mean")
         x = unpack_one(x, ps, "* c n")
         return x
@@ -637,22 +669,27 @@ class RT1(Module):
     def __init__(
         self,
         *,
-        vit: MaxViT,
-        num_actions=11,
-        action_bins=256,
-        depth=6,
-        heads=8,
-        dim_head=64,
-        token_learner_ff_mult=2,
-        token_learner_num_layers=2,
-        token_learner_num_output_tokens=8,
-        cond_drop_prob=0.2,
-        use_attn_conditioner=False,
-        conditioner_kwargs: dict = dict(),
+        vit: MaxViT,  # 视觉特征提取主干网络
+        num_actions=11,  # 输出动作维度
+        action_bins=256,  # 每个动作的离散化区间数
+        depth=6,  # Transformer 层的数量
+        heads=8,  # Transformer 的注意力头数
+        dim_head=64,  # 每个注意力头的维度
+        token_learner_ff_mult=2,  # TokenLearner 的 MLP 层维度倍数
+        token_learner_num_layers=2,  # TokenLearner 的 MLP 层数量，固定为 2
+        token_learner_num_output_tokens=8,  # TokenLearner 输出的 token 数量
+        cond_drop_prob=0.2,  # 条件函数的 dropout 概率
+        use_attn_conditioner=False,  # 是否使用注意力型条件控制器
+        conditioner_kwargs: dict = dict(),  # TextConditioner 的参数
     ):
+        """
+        输入：video: b c f h w, texts: list[str]
+        输出：b f num_actions action_bins
+        """
         super().__init__()
         self.vit = vit
 
+        # 获取 ViT 各个阶段的输入维度（用于条件控制）
         self.num_vit_stages = len(vit.cond_hidden_dims)
 
         conditioner_klass = (
@@ -660,7 +697,15 @@ class RT1(Module):
         )
 
         self.conditioner = conditioner_klass(
+            # 需要被控制的各个层的输入维度：
+            # - 前段：ViT 各个阶段的输入维度
+            # - 后段：Transformer 各个层的输入维度
+            # -- 由于 Transformer 的输入维度是固定的，因此只需要重复 Transformer 的输入维度 depth * 2 次
+            # -- Transformer = (TransformerAttention, FeedForward) * depth
             hidden_dims=(*tuple(vit.cond_hidden_dims), *((vit.embed_dim,) * depth * 2)),
+            # 各个层是否使用 channel first 的隐藏状态
+            # - ViT 的隐藏状态是 channel first 的 (B C H W)
+            # - Transformer 的隐藏状态是 channel last 的 (B Seq_len D)
             hiddens_channel_first=(
                 *((True,) * self.num_vit_stages),
                 *((False,) * depth * 2),
@@ -669,11 +714,13 @@ class RT1(Module):
             **conditioner_kwargs,
         )
 
+        # 输入为 vit 的最后一层输出的 token，不经过 mlp_head
+        # 对每一帧的所有 token 进行压缩，不管一帧输出多少个 token，都压缩为 token_learner_num_output_tokens 个 token
         self.token_learner = TokenLearner(
             dim=vit.embed_dim,
             ff_mult=token_learner_ff_mult,
             num_output_tokens=token_learner_num_output_tokens,
-            num_layers=token_learner_num_layers,
+            num_layers=token_learner_num_layers,  # always 2
         )
 
         self.num_learned_tokens = token_learner_num_output_tokens
@@ -705,13 +752,9 @@ class RT1(Module):
         text_embeds: Tensor | None = None,
         cond_drop_prob=0.0,
     ):
+        # 文本输入校验
         assert exists(texts) ^ exists(text_embeds)
-
-        if exists(texts):
-            num_texts = len(texts)
-        elif exists(text_embeds):
-            num_texts = text_embeds.shape[0]
-
+        num_texts = len(texts) if exists(texts) else text_embeds.shape[0]
         assert num_texts == video.shape[0], (
             f"you only passed in {num_texts} strings for guiding the robot actions, but received batch size of {video.shape[0]} videos"
         )
@@ -751,13 +794,20 @@ class RT1(Module):
         tokens = unpack_one(tokens, packed_shape, "* c h w")
         learned_tokens = self.token_learner(tokens)
 
+        # n = num_token_leanrner_output_token
+        # b0
+        # - f0_n0, f0_n1, ..., f0_n
+        # - f1_n0, f1_n1, ..., f1_n
+        # - ...
+        # - fF_n0, fF_n1, ..., fF_n
         learned_tokens = rearrange(learned_tokens, "b f c n -> b (f n) c")
 
         # causal attention mask
-
         attn_mask = torch.ones((frames, frames), dtype=torch.bool, device=device).triu(
             1
         )
+        # 注意 repeat 的顺序，r2 放在 j 的后面说明先将 j 维度上的每个元素重复 r2 次，如果 r2 放在 j 的前面则是先将 j 维度上的所有元素重复 r2 次
+        # 由于一个 frame 的所有 token 都共享一个 time_step 的信息，一个 frame 的所有 sim 的 mask 都是一样的
         attn_mask = repeat(
             attn_mask,
             "i j -> (i r1) (j r2)",
@@ -766,7 +816,6 @@ class RT1(Module):
         )
 
         # sinusoidal positional embedding
-
         pos_emb = posemb_sincos_1d(
             frames,
             learned_tokens.shape[-1],
@@ -774,17 +823,19 @@ class RT1(Module):
             device=learned_tokens.device,
         )
 
+        # 每个 frame 的所有 token (n个) 共享一个 time_step 的位置编码
         learned_tokens = learned_tokens + repeat(
             pos_emb, "n d -> (n r) d", r=self.num_learned_tokens
         )
 
         # attention
-
+        # 输出为 b f n d
         attended_tokens = self.transformer(
             learned_tokens, cond_fns=transformer_cond_fns, attn_mask=~attn_mask
         )
-
+        # 将一个 frame 的所有 token (n个) 求均值，得到一个 frame 的 token
         pooled = reduce(attended_tokens, "b (f n) d -> b f d", "mean", f=frames)
-
+        # output head, 最终输出 n_action 个 action_bins
         logits = self.to_logits(pooled)
+        # b, f, num_actions, action_bins
         return logits
